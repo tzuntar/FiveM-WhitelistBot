f23c3e5a7148939e93dcc65ee439b2e991b29930
RedCreator37 <tobija.zuntar@gmail.com>
2020-12-06 11:42:00 +0100

    Add localization support, localize all messages in the DiscordBot class

---
+++ src/main/java/com/redcreator37/WhitelistBot/Localizations.java
@@ -0,0 +1,51 @@
+package com.redcreator37.WhitelistBot;
+
+import java.util.Locale;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+/**
+ * Handles all localization-specific aspects of the bot
+ */
+public class Localizations {
+
+    /**
+     * The {@link ResourceBundle} object with all user-facing messages
+     */
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
+    private static final ResourceBundle strings =
+            getBundleFromLangCode("Strings", "en_US").get();
+
+    /**
+     * Returns the localized message from the message {@link ResourceBundle}
+     * with the matching key
+     *
+     * @param key the localization key
+     * @return the message (or <code>null</code> if the key isn't valid)
+     */
+    public static String lc(String key) {
+        return strings.getString(key);
+    }
+
+    /**
+     * Returns the {@link ResourceBundle} with the matching name for this
+     * language code
+     *
+     * @param baseName the full name of the {@link ResourceBundle} to retrieve
+     * @param langCode a code in the language_country format
+     *                 (ex. <code>en_US</code>)
+     * @return the matching {@link ResourceBundle} or an empty {@link Optional}
+     * if the matching bundle-language combination wasn't found
+     */
+    @SuppressWarnings("SameParameterValue")
+    private static Optional<ResourceBundle> getBundleFromLangCode(String baseName, String langCode) {
+        String[] locale = langCode.split("_");
+        try {
+            return Optional.of(ResourceBundle.getBundle(baseName, new Locale(locale[0]
+                    .toLowerCase(), locale[1].toUpperCase())));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+}
