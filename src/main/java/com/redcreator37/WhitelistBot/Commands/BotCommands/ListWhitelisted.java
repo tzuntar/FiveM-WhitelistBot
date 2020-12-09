ebf607243d143589e4e11d6e0861145e76d45ef8
RedCreator37 <tobija.zuntar@gmail.com>
2020-12-09 21:56:49 +0100

    Split the big execute method in ListWhitelisted class into smaller parts

--- src/main/java/com/redcreator37/WhitelistBot/Commands/BotCommands/ListWhitelisted.java
+++ src/main/java/com/redcreator37/WhitelistBot/Commands/BotCommands/ListWhitelisted.java
@@ -48,33 +48,60 @@ public class ListWhitelisted extends BotCommand {
                 .collect(Collectors.toCollection(Stack::new));
         for (int fieldsPerMessage = 0; fieldsPerMessage < 25; fieldsPerMessage++) {
             if (players.isEmpty()) break;
-            List<String> fields = new ArrayList<>();
-            int namesPerField = 10;
-            // filter player IDs into groups of 10
-            for (int i = 0; i < players.size(); i++) {
-                StringBuilder b = new StringBuilder(1024);
-                for (int j = 0; j < namesPerField && i < players.size(); j++) {
-                    b.append(players.pop()).append("\n");
-                    i++;
-                }
-                fields.add(b.toString().trim());
-            }
-            // add the fields with player IDs (10 per each field)
-            // and put them into an embed on the guild
-            int currentMessage = fieldsPerMessage;
-            channel.createEmbed(spec -> {
-                spec.setTitle(MessageFormat.format(lc("whitelisted-players-format"),
-                        currentMessage + 1, players.size() / 25));
-                spec.setColor(Color.YELLOW);
-                for (int i = 0; i < fields.size(); i++)
-                    for (int j = 0; j < 3 && i < fields.size(); j++) {
-                        spec.addField("`[" + j + 1 + "-3]`", fields.get(j), j != 2);
-                        i++;
-                    }
-                CommandUtils.setSelfAuthor(event.getGuild(), spec);
-                spec.setTimestamp(Instant.now());
-            }).block();
+            // add the fields into embeds (10 per each) and submit them
+            List<String> fields = splitInSize(players, 10);
+            submitFields(fields, channel, fieldsPerMessage, players.size(), event).block();
         }
         return Mono.empty();
+    }
+
+    /**
+     * Splits this {@link Stack} of {@link WhitelistedPlayer} objects
+     * into smaller units for easier distribution
+     *
+     * @param players       the stack of players to split
+     * @param namesPerField the number of names to add to each field
+     * @return the {@link List} of fields
+     */
+    private List<String> splitInSize(Stack<WhitelistedPlayer> players, int namesPerField) {
+        List<String> fields = new ArrayList<>();
+        for (int i = 0; i < players.size(); i++) {
+            StringBuilder b = new StringBuilder(1024);
+            for (int j = 0; j < namesPerField && i < players.size(); j++) {
+                b.append(players.pop()).append("\n");
+                i++;
+            }
+            fields.add(b.toString().trim());
+        }
+        return fields;
+    }
+
+    /**
+     * Submits these fields into an embed on the server
+     *
+     * @param fields     the list of fields to submit
+     * @param channel    the {@link MessageChannel} onto which to submit them
+     * @param currentMsg the number of the current message (used when
+     *                   splitting into multiple embeds)
+     * @param size       the number of all messages (used when
+     *                   splitting into multiple embeds)
+     * @param event      the {@link MessageCreateEvent} which occurred
+     *                   when the original message was sent
+     * @return an empty {@link Mono} object
+     */
+    private Mono<Void> submitFields(List<String> fields, MessageChannel channel, int currentMsg,
+                                    int size, MessageCreateEvent event) {
+        return channel.createEmbed(spec -> {
+            spec.setTitle(MessageFormat.format(lc("whitelisted-players-format"),
+                    currentMsg + 1, size / 25));
+            spec.setColor(Color.YELLOW);
+            for (int i = 0; i < fields.size(); i++)
+                for (int j = 0; j < 3 && i < fields.size(); j++) {
+                    spec.addField("`[" + j + 1 + "-3]`", fields.get(j), j != 2);
+                    i++;
+                }
+            CommandUtils.setSelfAuthor(event.getGuild(), spec);
+            spec.setTimestamp(Instant.now());
+        }).then();
     }
 }
